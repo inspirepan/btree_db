7db35fbeb9b2690627f0ce2f5435ee0604422e70
PanJixiang <narfnas@163.com>
2020-07-28 15:44:28 +0800

    update profiles

--- src/main/java/cn/edu/tsinghua/au/bioinfo/btree/App.java
+++ src/main/java/cn/edu/tsinghua/au/bioinfo/btree/App.java
@@ -3,13 +3,11 @@ package cn.edu.tsinghua.au.bioinfo.btree;
 import btree4j.BTree;
 import btree4j.BTreeException;
 
-import java.io.*;
-
 public class App {
     public static void main(String[] args) {
 
         try {
-            BTreeSearcher bm = new BTreeSearcher("E://Onedrive//code//btree_data");
+            BTreeSearcher btree = new BTreeSearcher("E://Onedrive//code//btree_data");
         } catch (BTreeException e) {
             e.printStackTrace();
         }
